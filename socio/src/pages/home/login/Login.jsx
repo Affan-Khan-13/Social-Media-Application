95f139dd2eeb8dcd709a84d7979fd024e2be9a37
unknown <indushassan@gmail.com>
2023-03-22 20:04:39 +0530

    my third commit

--- socio/src/pages/home/login/Login.jsx
+++ socio/src/pages/home/login/Login.jsx
@@ -1,6 +1,21 @@
+import { useContext, useRef } from "react";
 import "./login.css"
+import { loginCall } from "../../../apiCalls";
+import { AuthContext } from "../../../context/AuthContext";
+import PendingIcon from '@mui/icons-material/Pending';
 
-export default function Login() {
+export default function Login() 
+{
+    const email = useRef();
+    const password = useRef();
+    const {user, isFetching, error, dispatch} = useContext(AuthContext)
+
+    const handleClick = (e) =>{
+        e.preventDefault();
+        loginCall(
+            {email:email.current.value, password:password.current.value}, dispatch)
+    };
+    console.log(user)
   return (
     <div className="login">
         <div className="loginWrapper">
@@ -9,13 +24,22 @@ export default function Login() {
                 <span className="loginDesc">Connect With Friends around the world on Socio</span>
             </div>
             <div className="loginRight">
-                <div className="loginBox">
-                    <input placeholder="Email" className="loginInput" />
-                    <input placeholder="Password" className="loginInput" />
-                    <button className="loginButton">Log In</button>
+                <form className="loginBox" onSubmit={handleClick}>
+                    <input placeholder="Email" 
+                    type="email"
+                    required 
+                    className="loginInput" 
+                    ref={email}/>
+                    <input placeholder="Password" 
+                    type="password" 
+                    required 
+                    minLength="6"
+                    className="loginInput" 
+                    ref={password} />
+                    <button className="loginButton">{isFetching ? <PendingIcon/> :"Log In"}</button>
                     <span className="loginForgot">Forgot Password ?</span>
-                    <button className="loginRegisterButton">Create a New Account</button>
-                </div>
+                    <button className="loginRegisterButton">{isFetching ? <PendingIcon/> :"Create a new Account"}</button>
+                </form>
             </div>
         </div>
       
