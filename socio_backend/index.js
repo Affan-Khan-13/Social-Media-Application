805ef1aa402c1de4c39416b27c2f5d7d2adf3bf9
unknown <indushassan@gmail.com>
2023-03-25 02:12:13 +0530

    my fifth commit

--- socio_backend/index.js
+++ socio_backend/index.js
@@ -6,6 +6,8 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const multer = require("multer");
+const path = require("path");
 
 const app = express();
 
@@ -13,10 +15,33 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }).then(() => console.log('Connected to our MongoDB'))
     .catch(e => console.log(e))
 
+
+
+
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    try {
+      return res.status(200).json("File uploded successfully");
+    } catch (error) {
+      console.error(error);
+    }
+  });
 
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
